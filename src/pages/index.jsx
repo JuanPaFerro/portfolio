e6247c5ace6d2eb0dd19d929695febc3710daead
unknown <juanpabloferroarroyo@gmail.com>
2022-05-20 05:34:19 +0200

    Moved the Hello section to a component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,13 @@
-import Layout from "@components/Layout/Layout";
+import Hello from "@components/Hello/Hello";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <Layout>
-      <section className="w-full px-4 flex flex-col justify-between">
-        <div className="absolute w-44 h-48 left-4 top-60 bg-gradient-green opacity-40 blur-3xl" />
-        <div className="absolute w-44 h-48 right-4 bottom-60 bg-gradient-blue opacity-40 blur-3xl" />
-        <div className="text-white mt-[calc(20vh)]">
-          <p className="text-bodytext">Hi all. I am</p>
-          <p className="text-headline">Juan P. Ferro</p>
-          <p className="text-gradient-green text-bodytext">
-            {`>`} Front-end developer
-          </p>
-        </div>
-        <div className="text-snippets mt-[calc(22vh)]">
-          <p className="text-gray">{`//`} find my profile on Github:</p>
-          <div className="text-white">
-            <span className="text-gradient-blue">const</span>{" "}
-            <span className="text-gradient-green">githubLink</span> ={" "}
-            <a href="" className="text-orange hover:underline">
-              {`"https://github.com/example/url"`}
-            </a>
-          </div>
-        </div>
-      </section>
-    </Layout>
+    <>
+      <Head>
+        <title>juan-pablo-ferro</title>
+      </Head>
+      <Hello />
+    </>
   );
 }
