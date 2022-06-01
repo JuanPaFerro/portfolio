8c7ed1bc7be8b6f9325d671d9968e99f9bb59426
unknown <juanpabloferroarroyo@gmail.com>
2022-06-02 00:29:17 +0200

    Adjusted some styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from "@components/Footer/Footer";
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="min-h-screen w-full p-4 bg-black font-monospace">
+      <div className="h-screen w-full p-4 bg-black font-monospace overflow-auto">
         <div className="min-h-full w-full border border-lines rounded-lg bg-blue relative overflow-auto">
           <Nav />
           {children}
